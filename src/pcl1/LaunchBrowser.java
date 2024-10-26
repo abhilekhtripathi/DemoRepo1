5100898f3279e0026fa0bb7e4be729c354bd3fc0
Abhilekh <abhilekhtripathi8@gmail.com>
2024-10-26 23:06:49 +0530

    search page

---
+++ src/pcl1/LaunchBrowser.java
@@ -0,0 +1,11 @@
+package pcl1;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+	 // pubvoid(){
+		
+	}
+	}
+
+
---
+++ src/pcl1/Search.java
@@ -0,0 +1,10 @@
+package pcl1;
+
+public class Search {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
