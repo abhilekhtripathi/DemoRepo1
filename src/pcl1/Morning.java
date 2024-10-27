e35cad65a78707e62c0555513a9d9b150ae2611a
Abhilekh <abhilekhtripathi8@gmail.com>
2024-10-27 08:20:19 +0530

    array

---
+++ src/pcl1/Morning.java
@@ -0,0 +1,19 @@
+package pcl1;
+
+import java.util.HashSet;
+
+public class Morning {
+
+	public static void main(String[] args) {
+		
+			
+	int a []= {345,67,6,6,7,5,7,5,6};
+	  HashSet<Integer> hs = new HashSet<Integer>();
+	//  hs.add(a[i]);
+	  
+	  
+		
+
+	}
+
+}
